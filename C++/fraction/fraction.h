f452c6c5ca7e8307583ab6da7fd0f1749c1ccf6b
GhassenHafsiaINSAT <ghassen.hafsia@insat.ucar.tn>
2024-04-27 00:48:13 +0200

    adding the Makefiles

--- C++/fraction/fraction.h
+++ C++/fraction/fraction.h
@@ -15,6 +15,5 @@ class fraction
         int simplification(fraction& other);
         fraction operator+(const fraction& other) const; 
         friend std::ostream& operator<<(std::ostream& os, const fraction& other); 
-        friend fraction operator++(const fraction& other); 
 }; 
 
